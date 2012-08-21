0bc2df8a1930369bb14a820a7b6a15dd99357c21
Diego Dorado <diegodorado@gmail.com>
2012-08-21 05:48:01 -0300

    first commit

---
+++ source/javascripts/application.js
@@ -0,0 +1,10 @@
+// TIMESTAMP:2011-08-26 19:21:39 +0200
+// 
+// put coffee script and js source files into lib/javascripts or vendor/assets/javascripts
+//
+// =require 'jquery-1.7.2.min.js'
+// =require 'soundcloud.player.api.js'
+// =require 'sc-player.js'
+// =require 'app'
+
+
